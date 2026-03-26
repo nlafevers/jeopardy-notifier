f766d99e0a90b3ca112eb4b82da7254853f2f4ed
Nathan LaFevers <nathan.lafevers@gmail.com>
2026-03-26 03:49:56 -0400

    Changed form labels

--- core/forms.py
+++ core/forms.py
@@ -13,11 +13,11 @@ class TurnstileWidget(forms.Widget):
 
 
 class UploadForm(forms.Form):
-    hours_report = forms.FileField(label='Hours Report (Excel)')
-    roster = forms.FileField(label='Employee Roster (Excel)')
+    hours_report = forms.FileField(label='Hours Report')
+    roster = forms.FileField(label='Employee Roster')
     assignment = forms.CharField(label='Assignment to Rank', initial='Jeopardy 7a-7a')
     custom_message = forms.CharField(
-        label='Custom Message (Optional)',
+        label='Message',
         widget=forms.Textarea,
         required=False,
         initial='We have some unfilled jeopardy shifts in the next few weeks, please login to Qgenda and consider signing up if you are available.'
